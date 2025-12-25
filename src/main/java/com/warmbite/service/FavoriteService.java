f7feccde0e814ef0ce5a931ce143c78b3fd9828c
Potato-Chen1122 <231250060@smail.nju.edu.cn>
2025-12-25 23:19:08 +0800

    添加跨域配置

--- src/main/java/com/warmbite/service/FavoriteService.java
+++ src/main/java/com/warmbite/service/FavoriteService.java
@@ -16,21 +16,24 @@ public class FavoriteService {
     private FavoriteRepository favoriteRepository;
 
     public Set<String> getFavoriteIds(String userId) {
-        if(favoriteRepository.findByUserId(Long.valueOf(userId)).isPresent())
+        if(favoriteRepository.findByUserId(Long.valueOf(userId)).isPresent()) {
+            System.out.printf("[#]userId %s get\n",userId);
             return favoriteRepository.findByUserId(Long.valueOf(userId)).get().stream().map((Favorite::getRecipeId)).map(String::valueOf).collect(Collectors.toSet());
-        return new HashSet<>();
+        }return new HashSet<>();
     }
 
     public int toggleFavorite(String userId,String recipeId) {
         if(favoriteRepository.findToggle(Long.valueOf(userId),Long.valueOf(recipeId)).isPresent()){
             Favorite favorite=favoriteRepository.findToggle(Long.valueOf(userId),Long.valueOf(recipeId)).get();
             favoriteRepository.delete(favorite);
+            System.out.printf("[#]userId %s remove %s\n",userId,recipeId);
             return 0;
         }else {
             Favorite favorite=new Favorite();
             favorite.setUserId(Long.valueOf(userId));
             favorite.setRecipeId(Long.valueOf(recipeId));
             favoriteRepository.save(favorite);
+            System.out.printf("[#]userId %s favorite %s\n",userId,recipeId);
             return 1;
         }
     }
